dc830b2c553a7380f5c42b9db75eb8bc1253dde1
pampfelimetten <pampfelimetten@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2013-07-23 16:13:42 +0000

    Lets get this party started. Release version 1.0.
    git-svn-id: https://plugins.svn.wordpress.org/cba-easy-embed/trunk@745052 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ cba-oembed.php
@@ -0,0 +1,28 @@
+<?php
+
+/* 
+	Plugin Name: CBA Easy Embed
+	Plugin URI: https://cba.fro.at
+	Description: Makes it easier to embed content from the Cultural Broadcasting Archive
+	Author: Thomas Diesenreiter and Ingo Leindecker
+	Version: 1.0.0
+	Author URI: http://cba.fro.at
+	
+  This program is free software; you can redistribute it and/or modify
+  it under the terms of the GNU General Public License, version 2, as 
+  published by the Free Software Foundation.
+
+  This program is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  GNU General Public License for more details.
+
+  You should have received a copy of the GNU General Public License
+  along with this program; if not, write to the Free Software
+  Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+
+*/
+
+wp_oembed_add_provider( 'http://cba.fro.at/*', 'http://cba.fro.at/?oembed=true' );
+
+?>
---
+++ readme.txt
@@ -0,0 +1,27 @@
+=== CBA Easy Embed ===
+Contributors: pampfelimetten
+Tags: oembed, embed, cba, cultural broadcasting archive
+Requires at least: 3.0
+Tested up to: 3.5.2
+Stable tag: trunk
+License: GPLv2 or later License
+License URI: http://www.gnu.org/licenses/gpl-2.0.html
+Donate link: http://flattr.com/profile/culturalbroadcastingarchive
+
+== Description ==
+
+Makes it easier to embed content from the Cultural Broadcasting Archive (http://cba.fro.at). Paste in a link to a single entry like http://cba.fro.at/112836, and the plugin will get the default embed code for you. Uses the oembed technology.
+
+== Installation ==
+
+1. Upload the plugin to the `/wp-content/plugins/` directory 2. Activate the plugin through the 'Plugins' menu in WordPress 3. Your done. Happy embedding!
+
+== Frequently Asked Questions ==
+
+If anything is unclear, please contact us via http://cba.fro.at
+
+== Changelog ==
+
+= 1.0 =
+
+initial release
